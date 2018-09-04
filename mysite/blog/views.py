407d6e56f46e1e9a10eb27b166bfcaf4c46b4a50
Keith Lashley <lashleykeith@gmail.com>
2018-09-04 14:12:49 +0900

    Creating Template Views
    Creating Template Views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,7 +9,15 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
 									status='published',
-									publish_year=year,
-									publish_month=month,
-									publish_day=day)
+									publish__year=year,
+									publish__month=month,
+									publish__day=day)
 	return render(request, 'blog/post/detail.html',{'post':post})
+
+'''
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+'''
