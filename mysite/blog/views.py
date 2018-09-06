ae5d23a834d1761d7e47da398b480e70c07693e4
Keith Lashley <lashleykeith@gmail.com>
2018-09-06 19:14:14 +0900

    Using class based views
    Using class based views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 def post_list(request):
@@ -17,6 +18,12 @@ def post_list(request):
 		posts = paginator.page(paginator.num_pages)
 	return render(request,'blog/post/list.html',{'page': page,
 												'posts': posts})
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
